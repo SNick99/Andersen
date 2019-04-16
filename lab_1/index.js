0de98973cd24e9e60a0ad81fe5ca41ed1443d782
Nikolay Sibibert <sibibert2025@gmail.com>
2019-04-16 09:08:15 +0300

    исправил свою второю задачу

--- lab_1/index.js
+++ lab_1/index.js
@@ -27,26 +27,19 @@ let Func = function(arr) {
   }
 };
 
-Func([{ a: null, b: 5 }, { a: null, b: 5 }, { a: null, b: 5 }]);
-Func([{ a: null, b: 5 }, { a: null, b: 5 }, { a: 1, b: 5 }]);
+// Func([{ a: null, b: 5 }, { a: null, b: 5 }, { a: null, b: 5 }]);
+// Func([{ a: null, b: 5 }, { a: null, b: 5 }, { a: 1, b: 5 }]);
 
 ///parse("Привет,   как дела?") // ["дела", "как",  "Привет"]
 
 let parse = function(str) {
-  let arr = str
-    .trim()
-    .replace(/ +|\.|\?|!|,|-/g, " ")
-    .split(" ");
+  let arr = str.match(/[\wа-я]+/gim).sort(function(a, b) {
+    return a.localeCompare(b);
+  });
 
-  let arr2 = arr
-    .filter(a => {
-      return a != "";
-    })
-    .sort(function(a, b) {
-      return a.localeCompare(b);
-    });
-
-  console.log(arr2);
+  console.log(arr);
 };
 
-parse("Привет,  как дела?");
+parse(`Привет,  как дела?
+ Снова привет, как дела?
+`);
