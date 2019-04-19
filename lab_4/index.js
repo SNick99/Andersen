1aae4a732dd2c3ff8c6d841a12a6059acb6555e6
Nikolay Sibibert <sibibert2025@gmail.com>
2019-04-19 17:08:24 +0300

    PromiseRase completed

--- lab_4/index.js
+++ lab_4/index.js
@@ -1,34 +1,71 @@
 //1) Написать PromiseAll(arrOfPromise)
 var p1 = Promise.resolve(3);
-var p2 = 1337;
+var p2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 400, "nefoo");
+});
 var p3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "foo");
+  setTimeout(resolve, 100, "foo1");
 });
 
 const PromiseAll = arr => {
-  let ArrayResult = [];
-  let arrPromises = arr.map(element => {
-    if (!(element instanceof Promise)) {
-      return Promise.resolve(element);
-    } else {
-      return element;
-    }
-  });
-
-  arrPromises.forEach(element => {
-    element.then(
-      res => {
-        ArrayResult.push(res);
-        if (ArrayResult.length === arr.length) {
-          console.log(ArrayResult);
-          return ArrayResult;
-        }
-      },
-      error => {
-        console.log(error.message);
+  return new Promise((resolve, reject) => {
+    let ArrayResult = [arr.length];
+    let arrPromises = arr.map(element => {
+      if (!(element instanceof Promise)) {
+        return Promise.resolve(element);
+      } else {
+        return element;
       }
-    );
+    });
+
+    arrPromises.forEach((element, i) => {
+      element.then(
+        res => {
+          ArrayResult[i] = res;
+
+          if (Object.keys(ArrayResult).length === arr.length) {
+            resolve(ArrayResult);
+          }
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
   });
 };
 
-// PromiseAll([p1, p2, p3]);
+// PromiseAll([p1, p2, p3]).then(val => {
+//   console.log(val);
+// });
+
+//========================================================================
+// 2) Написать PromiseRase(arrOfPromise)
+//1) Написать PromiseAll(arrOfPromise)
+
+const PromiseRase = arr => {
+  return new Promise((resolve, reject) => {
+    let arrPromises = arr.map(element => {
+      if (!(element instanceof Promise)) {
+        return Promise.resolve(element);
+      } else {
+        return element;
+      }
+    });
+
+    arrPromises.forEach((element, i) => {
+      element.then(
+        res => {
+          resolve(res);
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
+  });
+};
+
+// PromiseRase([p2, p3]).then(val => {
+//   console.log(val);
+// });
